7d983bfb36243d55134d4eebdc2170a7207193fc
Kashyap <kashyap.kuchipudi@gmail.com>
2026-01-14 17:11:24 -0500

    next-tiptap-sourcecode added to the repository. This will be completely cleaned up later.

---
+++ next-tiptap-sourcecode/src/app/layout.tsx
@@ -0,0 +1,48 @@
+import { Metadata } from "next";
+import { JetBrains_Mono, Open_Sans } from "next/font/google";
+
+const fontMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
+const fontSans = Open_Sans({ subsets: ["latin"], variable: "--font-sans" });
+
+import Header from "@/components/shared/header";
+
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Next Tiptap",
+  description:
+    "A modern WYSIWYG rich text editor based on tiptap and shadcn ui for ReactJs/NextJs",
+  keywords: "Tiptap, WYSIWYG, Rich Text Editor, ReactJS, NextJS",
+  metadataBase: new URL(`https://next-tiptap.vercel.app`),
+  openGraph: {
+    type: "website",
+    url: `https://next-tiptap.vercel.app`,
+    title: "Next Tiptap",
+    description:
+      "A modern WYSIWYG rich text editor based on tiptap and shadcn ui for ReactJs/NextJs",
+    siteName: "Next Tiptap",
+    locale: "en_US",
+    images: "/opengraph-image.jpg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${fontMono.variable} ${fontSans.variable} antialiased`}
+    >
+      <body>
+        <Header />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
