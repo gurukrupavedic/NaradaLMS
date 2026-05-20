f2ec2851f114e99b34ee7f990055504c71d0eb87
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2026-05-19 21:37:02 -0700

    chore: pretty print logs in development

--- apps/api/src/logger.ts
+++ apps/api/src/logger.ts
@@ -1,5 +1,19 @@
 import pino from 'pino'
 
-const logger = pino()
+function getTransport(): pino.LoggerOptions<never, boolean> {
+  if (process.env.NODE_ENV === 'production') {
+    return {}
+  }
 
+  return {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    },
+  }
+}
+
+const logger = pino(getTransport())
 export default logger
