ccbc3ed4075227d5d7766634ebeff198afc9ed61
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2026-06-14 12:00:42 -0700

    fix(auth): append /auth to client baseURL

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -3,5 +3,5 @@
 import { env } from '@narada/env'
 import { createAuthClient } from 'better-auth/react'
 
-export const authClient = createAuthClient({ baseURL: env.NEXT_PUBLIC_API_URL })
+export const authClient = createAuthClient({ baseURL: `${env.NEXT_PUBLIC_API_URL}/auth` })
 export const { useSession, signIn, signUp, signOut } = authClient
